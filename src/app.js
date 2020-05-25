ac605e5c4955ba4f72d8f3128d0275c64d5fce8f
Karan Shah <karanshah0206@outlook.com>
2020-05-25 11:15:49 +0530

    Init

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-let express = require('express');
-let app = express();
-let server = require('http').Server(app);
-let io = require('socket.io')(server);
-let stream = require('./ws/stream');
-let path = require('path');
-let favicon = require('serve-favicon')
+// Initialize
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+var stream = require('./ws/stream');
+var path = require('path');
+const port = 3000;
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+// Passing UI
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.get('/', (req, res)=>{res.sendFile(__dirname+'/index.html');});
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
-});
-
-
-io.of('/stream').on('connection', stream);
-
-server.listen(3000);
+// Establish Connection
+try {
+    io.of('/stream').on('connection', stream);
+    server.listen(port);
+    console.log('Listening On Port ' + port);
+} catch (err) {
+    console.log('Unable To Established Connection On Port ' + port + '. Error Code: ' + err);
+}
