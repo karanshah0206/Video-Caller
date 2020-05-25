9b53bb9abc64cab4f485887d0aa8f2e9560a552b
Karan Shah <karanshah0206@outlook.com>
2020-05-25 12:17:41 +0530

    Documentation

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -103,7 +103,7 @@ export default {
         }
     },
 
-
+    // Ice Server
     getIceServer(){
         return {
             iceServers: [
@@ -111,17 +111,16 @@ export default {
                     urls: ["stun:eu-turn4.xirsys.com"]
                 }, 
                 {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl", 
+                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
                     credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
                     urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",                         
+                        "turn:eu-turn4.xirsys.com:80?transport=udp",
                         "turn:eu-turn4.xirsys.com:3478?transport=tcp"
                     ]
                 }
             ]
         };
     },
-    
     
     addChat(data, senderType){
         let chatMsgDiv = document.querySelector('#chat-messages');
